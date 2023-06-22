aa2eeff0a5d58b951183b24e60393dba2ff2476a
Zef Hemel <zef@zef.me>
2023-06-22 09:22:48 +0200

    Github clone support

--- README.md
+++ README.md
@@ -2,17 +2,21 @@
 
 Very basic in functionality, it assumes you have git configured for push and pull in your space. What it does, roughly speaking:
 
-`Git : Sync`:
+{[Git: Sync]}:
 
-- Adds all *.md files in your folder to git
+- Adds all files in your folder to git
 - It commits them with a "Snapshot" commit message
 - It `git pull`s changes from the remote server
 - It `git push`es changes to the remote server
 
-`Git: Snapshot`:
+{[Git: Snapshot]}:
 
 - Asks you for a commit message
 - Commits
+
+{[Github: Clone]}:
+
+Clones into your space from a Github repository. This will do authentication based on a [personal access token](https://github.com/settings/tokens).
 
 ## Installation
 
--- git.ts
+++ git.ts
@@ -1,5 +1,5 @@
 import { shell } from "$sb/plugos-syscall/mod.ts";
-import { editor, system } from "$sb/silverbullet-syscall/mod.ts";
+import { editor } from "$sb/silverbullet-syscall/mod.ts";
 
 export async function commit(message?: string) {
   if (!message) {
@@ -9,7 +9,7 @@ export async function commit(message?: string) {
     "Snapshotting the current space to git with commit message",
     message,
   );
-  const { code } = await shell.run("git", ["add", "./*.md"]);
+  const { code } = await shell.run("git", ["add", "./*"]);
   console.log("Git add code", code);
   try {
     await shell.run("git", ["commit", "-a", "-m", message]);
@@ -25,16 +25,12 @@ export async function snapshotCommand() {
     revName = "Snapshot";
   }
   console.log("Revision name", revName);
-  await system.invokeFunction("server", "commit", revName);
+  await commit(revName);
+  await editor.flashNotification("Done!");
 }
 
 export async function syncCommand() {
   await editor.flashNotification("Syncing with git");
-  await system.invokeFunction("server", "sync");
-  await editor.flashNotification("Git sync complete!");
-}
-
-export async function sync() {
   console.log("Going to sync with git");
   await commit();
   console.log("Then pulling from remote");
@@ -42,4 +38,40 @@ export async function sync() {
   console.log("And then pushing to remote");
   await shell.run("git", ["push"]);
   console.log("Done!");
+  await editor.flashNotification("Git sync complete!");
+}
+
+export async function githubCloneCommand() {
+  let url = await editor.prompt(`Github project URL:`);
+  if (!url) {
+    return;
+  }
+  const token = await editor.prompt(`Github token:`);
+  if (!token) {
+    return;
+  }
+  const name = await editor.prompt(`Your name:`);
+  if (!name) {
+    return;
+  }
+  const email = await editor.prompt(`Your email:`);
+  if (!email) {
+    return;
+  }
+  const pieces = url.split("/");
+  pieces[2] = `${token}@${pieces[2]}`;
+  url = pieces.join("/") + ".git";
+  await editor.flashNotification(
+    "Now going to clone the project, this may take some time.",
+  );
+  await shell.run("mkdir", ["-p", "_checkout"]);
+  await shell.run("git", ["clone", url, "_checkout"]);
+  // Moving all files from _checkout to the current directory, which will complain a bit about . and .., but we'll ignore that
+  await shell.run("sh", ["-c", "mv _checkout/{.,}* . 2> /dev/null; true"]);
+  await shell.run("rm", ["-rf", "_checkout"]);
+  await shell.run("git", ["config", "user.name", name]);
+  await shell.run("git", ["config", "user.email", email]);
+  await editor.flashNotification(
+    "Done. Now just wait for sync to kick in to get all the content.",
+  );
 }
